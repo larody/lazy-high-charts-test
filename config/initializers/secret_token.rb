7a521001ff3b1ae5260a7817bfce528f05e803ce
larody <throwbases@gmail.com>
2013-05-12 17:53:35 +0900

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LazyHighChartsTest::Application.config.secret_token = '3d7dde399cbfa39955c628bf437518316cc95fe5b7547d65b06a85f071f4e81d5db6c14fa76b01881481315dcffb23756dfb3d5929f800701d4bcb2b201363a1'
